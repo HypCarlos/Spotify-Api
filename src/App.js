dcd1cf586cd88380b9463b4dff24102af3c01705
unknown <ccord042@fiu.edu>
2022-09-19 04:34:10 -0400

    added fetch, useEffect, useState for api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+
+const CLIENT_ID = "324e5602e5dd4c5a8f75c594f274413c";
+const CLIENT_SECRET = "630c21c1010f46889dbd5904df0a2b50";
 
 function App() {
+
+  const [searchInput, setSearchInput] = useState("");
+  // GETTING TOKEN AFTER USEEFFECT AND THEN USING STATE TO SAVE IT
+  const [accessToken, setAccessToken] = useState("");
+  
+
+
+  useEffect(() => {
+  
+    var authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, 
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+    .then(result => result.json())
+    // ACCESS TO TOKEN AS STRING USE.ACCESS_TOKEN
+    .then(data => setAccessToken(data.access_token))
+  },[])
+
   return (
     <div className="App">
-    <h1>Spotify</h1>
+    
+    
+    <Container>
+    <h1>SPOTIFY API</h1>
+      <InputGroup className='mb-3' size= 'lg'>
+        <FormControl 
+          placeholder='Search Artists'
+          type= 'input'
+          onKeyPress={event => {
+            if (event.key === "Enter") {
+              console.log("ENTER");
+            }
+          }}
+       onChange= {(event) => setSearchInput(event.target.value)}
+        />
+      <Button onClick = {() => {console.log("clicked button")}}>Search</Button>
+       
+      </InputGroup>
+    </Container>
+
+    <Container>
+    <Row className='mx-2 row row-cols-4'>
+    <Card>
+        <Card.Img src = '#'/>
+        <Card.Body>
+          <Card.Title>
+            Album name
+          </Card.Title>
+        </Card.Body>
+      </Card>
+
+  
+    </Row>
+   
+    </Container>
     </div>
   );
 }
 
 export default App;
+ 
